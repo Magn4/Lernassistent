0c407c90faa5d1e7db69774c9a2346ea3dca3c5d
Magn4 <tahatoo25@gmail.com>
2024-12-22 17:06:14 +0100

    Reloading Project

--- LernAssistent/Entities/User.cs
+++
@@ -1,9 +0,0 @@
-namespace LernAssistent.Entities
-{
-    public class User
-    {
-        public string? UserId { get; set; }
-        public int Id { get; set; }
-        public int Credits { get; set; }
-    }
-}
--- LernAssistent/Interfaces/IAiService.cs
+++
@@ -1,8 +0,0 @@
-
-namespace LernAssistent.Interfaces
-{
-    public interface IAiService
-    {
-        Task<string> ProcessTextAsync(byte[] pdfData);
-    }
-}
